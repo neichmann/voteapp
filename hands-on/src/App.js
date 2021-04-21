879072b77b59fe8ebfb31673bcc3c7b8a55bd780
NiklasT <eichi@magenta.de>
2021-04-21 11:33:49 +0200

    added App.js

---
+++ hands-on/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function App() {
+    return (
+        <div className="Background">
+            <div className="Header">
+                <div className="Title">
+                    Vote as a service
+                </div>
+            </div>
+
+            <div className="Main">
+                <div className="Container">
+                    { /*Hier wird reingerendert */ }
+                </div>
+            </div>
+        </div>
+    );
+}
